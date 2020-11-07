d6235d5e6654771b79bc08b6dbb5b31fcde6841e
Raz Al-Jaf <razaljaf@gmail.com>
2020-11-07 19:09:09 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,1 +1,17 @@
-# Write your code here
+2.6.1 :001 > count=10
+ => 10
+2.6.1 :002 > while count >=1 do
+2.6.1 :003 >     puts count
+2.6.1 :004?>   count-=1
+2.6.1 :005?>   end
+10
+9
+8
+7
+6
+5
+4
+3
+2
+1
+ => nil
